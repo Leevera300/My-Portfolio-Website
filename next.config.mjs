622fd0fb4dd9ebb724bdcce8a53608e7a9729e2c
Leevera300 <michaelrivera0206@gmail.com>
2025-06-10 16:47:45 +0900

    github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true, // required if you use <Image />
+  },
+  basePath: "/My-Portfolio-Website",
+  assetPrefix: "/My-Portfolio-Website/",
+};
 
 export default nextConfig;
